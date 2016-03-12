782a3ef9530cb8e473ceb074465771460c0ca8ce
tscheys <tim.scheys@gmail.com>
2016-03-12 18:37:35 +0100

    correct formatting  of output json

--- awsjson.sh
+++ awsjson.sh
@@ -1,6 +1,6 @@
-declare -a instances=("m1.small"  "m1.medium" "m1.large" "m1.xlarge" "m3.medium")
-declare -a filenames=("m1small"  "m1medium" "m1large" "m1xlarge" "m3medium")
-declare -a zones=("us-west-2" "us-west-1" "eu-west-1" "eu-central-1" "ap-southeast-1" "ap-northeast-1" "ap-southeast-2" "ap-northeast-2")
+declare -a instances=("m1.small"  "m3.medium")
+declare -a filenames=("m1small" "m3medium")
+declare -a zones=("us-west-2" "us-west-1")
 
 # get length of an array
 instancelength=${#instances[@]}
@@ -12,8 +12,8 @@ do
   aws configure set default.region ${zones[$j-1]}
   for (( i=1; i<${instancelength}+1; i++ ));
   do
-    aws ec2 describe-spot-price-history --instance-types ${instances[$i-1]}
-  done >> aws${zones[$j-1]}.json
+    aws ec2 describe-spot-price-history --instance-types ${instances[$i-1]} --product-description "Linux/UNIX" | jq -c ".SpotPriceHistory[]"
+  done >> aws2.json
 done
 
 
