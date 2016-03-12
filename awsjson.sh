a21c62f8772037e8d0f3b4e7f1dd68dc68ed9f30
tscheys <tim.scheys@gmail.com>
2016-03-12 20:02:34 +0100

    add comments to script

--- awsjson.sh
+++ awsjson.sh
@@ -1,23 +1,28 @@
+#declare instance types and zones to extract
 declare -a instances=("m1.small"  "m3.medium")
 declare -a filenames=("m1small" "m3medium")
 declare -a zones=("us-west-2" "us-west-1")
 
-# get length of an array
+# get number of instances, zones
 instancelength=${#instances[@]}
 zonelength=${#zones[@]}
 
-# use for loop to read all values and indexes
+# loop through zones
 for (( j=1; j<${zonelength}+1; j++ ));
 do
+  # configure default region to zone in loop
   aws configure set default.region ${zones[$j-1]}
   for (( i=1; i<${instancelength}+1; i++ ));
   do
+    # extract data in json format of current region, instance in loop
+    # use jq to make json compact, to be usable later in apache spark 
     aws ec2 describe-spot-price-history --instance-types ${instances[$i-1]} --product-description "Linux/UNIX" | jq -c ".SpotPriceHistory[]"
+    # append data to outputted json file
   done >> aws2.json
 done
 
 
-# > ${zones[$j-1]}/${temp}.json
+# aborts run with ctrl-c
 control_c() {
     kill $PID
     exit
