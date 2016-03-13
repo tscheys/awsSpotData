f6e39b81d9e5a695fc7f13fd7e6926f7f07ac243
tscheys <tim.scheys@gmail.com>
2016-03-13 11:19:06 +0100

    extract data per day

--- awsjson.sh
+++ awsjson.sh
@@ -2,24 +2,17 @@
 declare -a instances=("m1.small"  "m3.medium")
 declare -a filenames=("m1small" "m3medium")
 declare -a zones=("us-west-2" "us-west-1")
-
-# get number of instances, zones
-instancelength=${#instances[@]}
-zonelength=${#zones[@]}
-
-# get date 90 days ago 
-#90dayInteger=$(date -j -v-90d +"%s")
-#90dayYear=$(date -j -v-90d +"%Y")
-#90dayMonth=$(date -j -v-90d +"%m")
-#90dayDay=$(date -j -v-90d +"%d")
-
 declare -a dates=() 
-
-#create arrays 
+#create array of past 90 days 
 for ((days=89; days>0; days--));
 do
   dates+=($(date -j -v-${days}d +%Y-%m-%d))
 done
+
+# get number of instances, zones, dates
+instancelength=${#instances[@]}
+zonelength=${#zones[@]}
+dateslength=${#dates[@]}
 
 # loop through zones
 for (( j=1; j<${zonelength}+1; j++ ));
@@ -28,11 +21,15 @@ do
   aws configure set default.region ${zones[$j-1]}
   for (( i=1; i<${instancelength}+1; i++ ));
   do
+    #loop over past 90 days 
+    for ((k=1; k<${dateslength}+1; k++));
+    do
     # extract data in json format of current region, instance in loop
     # use jq to make json compact, to be usable later in apache spark 
-    aws ec2 describe-spot-price-history --instance-types ${instances[$i-1]} --product-description "Linux/UNIX" | jq -c ".SpotPriceHistory[]"
+    aws ec2 describe-spot-price-history --instance-types ${instances[$i-1]} --product-description "Linux/UNIX" --start-time ${dates[$k-1]}T00:00:00.000Z --end-time ${dates[$k-1]}T23:59:59.000Z | jq -c ".SpotPriceHistory[]"
     # append data to outputted json file
-  done >> aws2.json
+    done >> aws2.json
+  done
 done
 
 
@@ -41,8 +38,3 @@ control_c() {
     kill $PID
     exit
 }
-
-# get current date
-# subtract 90 days 
-# do api call for all those days 
-# so increment by one 
